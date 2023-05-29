ec13d0c067948d0d28278690c1ea37c6cc2edd48
Tarek <Doudou.vzy@gmail.com>
2023-05-29 23:07:06 +0200

    ajout de la fonction sedepalcer

--- src/Unite.java
+++ src/Unite.java
@@ -1,18 +1,22 @@
-public abstract class Unite {
+public abstract class Unite extends Component{
     private String nom;
     private String type;
     protected Outil outil;
     private int vitesseDeplacement;
     private int coutNourriture;
+    protected int positionX;
+    protected int positionY;
 
-    public Unite(String nom, String type,Outil outil, int vitesseDeplacement, int coutNourriture) {
+    public Unite(String nom, String type,Outil outil, int vitesseDeplacement, int coutNourriture, int positionX, int positionY) {
         this.nom = nom;
         this.type = type;
         this.outil = outil;
         this.vitesseDeplacement = vitesseDeplacement;
         this.coutNourriture = coutNourriture;
+        this.positionX=positionX;
+        this.positionY=positionY;
     }
-    public abstract void recuprerRessource(Carte carte, CoffreJoueur coffreJoueur, int x, int y);
+    public abstract void recupererRessource(Carte carte, CoffreJoueur coffreJoueur, int x, int y);
     public String getNom() {
         return nom;
     }
@@ -32,5 +36,38 @@ public abstract class Unite {
         int nourritureDisponible = joueur.getCoffre().getNourritureDisponible();
         return nourritureDisponible >= coutNourriture;
     }
-
+    public void seDeplacer(Carte carte){
+        seDeplacerBas(carte);
+        seDeplacerHaut(carte);
+        seDeplacerDroite(carte);
+        seDeplacerGauche(carte);
+    }
+    public void seDeplacerHaut(Carte carte){
+        if(carte.estPositionValide(this.positionX-1,this.positionY)){
+            carte.getCase(this.positionX,this.positionY).removeUnite(this);
+            this.positionX--;
+            carte.getCase(this.positionX,this.positionY).addUnite(this);
+        }
+    }
+    public void seDeplacerBas(Carte carte){
+        if(carte.estPositionValide(this.positionX+1,this.positionY)){
+            carte.getCase(this.positionX,this.positionY).removeUnite(this);
+            this.positionX++;
+            carte.getCase(this.positionX,this.positionY).addUnite(this);
+        }
+    }
+    public void seDeplacerDroite(Carte carte){
+        if(carte.estPositionValide(this.positionX,this.positionY+1)){
+            carte.getCase(this.positionX,this.positionY).removeUnite(this);
+            this.positionY++;
+            carte.getCase(this.positionX,this.positionY).addUnite(this);
+        }
+    }
+    public void seDeplacerGauche(Carte carte){
+        if(carte.estPositionValide(this.positionX,this.positionY-1)){
+            carte.getCase(this.positionX,this.positionY).removeUnite(this);
+            this.positionY--;
+            carte.getCase(this.positionX,this.positionY).addUnite(this);
+        }
+    }
 }
